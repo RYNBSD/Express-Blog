319e8e90eb0e369665bf3ed59adc11f5c45731f3
RYN BSD <rynbsd04@gmail.com>
2024-01-20 17:08:55 +0100

    config db schema

--- ts/src/schema/db.ts
+++ ts/src/schema/db.ts
@@ -8,5 +8,36 @@ const IdInt = z.object({
 }).strict()
 
 export const User = z.object({
-    username: z.string(),
-}).strict()
+    username: z.string().trim().max(50),
+    email: z.string().trim().max(50),
+    password: z.string().trim().max(70),
+    picture: z.string().trim().max(70)
+}).merge(IdUUID).strict()
+
+export const Blog = z.object({
+    title: z.string().trim().max(255),
+    description: z.string().trim().max(1000),
+    bloggerId: z.string().trim().uuid(),
+}).merge(IdUUID).strict()
+
+export const BlogImages = z.object({
+    image: z.string().trim().max(70),
+    blogId: z.string().trim().uuid()
+}).merge(IdInt).strict()
+
+export const BlogLikes = z.object({
+    blogId: z.string().trim().uuid(),
+    likerId: z.string().trim().uuid(),
+}).merge(IdInt).strict()
+
+export const BlogComments = z.object({
+    comment: z.string().trim().max(255),
+    blogId: z.string().trim().uuid(),
+    commenterId: z.string().trim().uuid()
+}).merge(IdInt).strict()
+
+export const Error = z.object({
+    status: z.number().min(100).max(599),
+    message: z.string().trim().max(255),
+    isFixed: z.boolean()
+}).merge(IdInt).strict()
