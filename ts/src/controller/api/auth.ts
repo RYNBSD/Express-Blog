d891d1c5e8917b4d997f6e3df80a971a3e6f8236
RYN BSD <rynbsd04@gmail.com>
2024-01-21 19:42:38 +0100

    set sign-out controller

--- ts/src/controller/api/auth.ts
+++ ts/src/controller/api/auth.ts
@@ -2,27 +2,42 @@ import type { Request, Response } from "express"
 import { schema } from "../../schema/index.js"
 import { util } from "../../util/index.js"
 import { StatusCodes } from "http-status-codes"
+import { model } from "../../model/index.js"
 
 export const auth =  {
     async signUp(req: Request, res: Response) {
+        const picture = req.file
+        if (picture === null)
+            throw new Error("Picture required")
+        
         const { SignUp } = schema.req.auth
         const { username, email, password } = SignUp.parse(req.body) 
 
         const { bcrypt } = util
         const hash = bcrypt.hash(password)
 
+        const { User } = model.db
+        await User.create()
+
         res.status(StatusCodes.CREATED)
     },
     async signIn(req: Request, res: Response) {
 
+        res.status(StatusCodes.OK);
     },
     async signOut(req: Request, res: Response) {
+        req.session.access = { key: "", iv: "" }
+        req.session.csrf = { secret: "" }
+        req.session.user = { id: "" }
 
+        res.status(StatusCodes.OK);
     },
     async me(req: Request, res: Response) {
 
+        res.status(StatusCodes.OK);
     },
     async forgotPassword(req: Request, res: Response) {
 
+        res.status(StatusCodes.OK);
     },
 } as const
---
+++ ts/src/model/index.ts
@@ -0,0 +1,5 @@
+const [db] = await Promise.all([import("./db.js")])
+
+export const model = {
+    db,
+} as const
