522bcfa727539310484b78d5481535e5f412f987
RYN BSD <rynbsd04@gmail.com>
2024-01-21 13:01:27 +0100

    init routers

--- ts/src/router/blog.ts
+++ ts/src/router/blog.ts
@@ -1,3 +1,19 @@
 import { Router } from "express";
 
 export const blog = Router()
+
+blog.get("/all")
+
+blog.get("/:blogId")
+
+blog.get("/:blogId/likes")
+
+blog.get("/:blogId/comments")
+
+blog.patch("/like")
+
+blog.post("/")
+
+blog.put("/:blogId")
+
+blog.delete("/:blogId")
--- ts/src/router/user.ts
+++ ts/src/router/user.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
-export const user = Router()
+export const user = Router();
 
-user.get("")
+user.get("/:userId/info");
+
+user.get("/:userId/blogs");
+
+user.patch("/follow");
+
+user.put("/:userId");
+
+user.delete("/:userId");
