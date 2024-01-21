570c0889eda747c0d4306c689775eae5dcfc11fa
RYN BSD <rynbsd04@gmail.com>
2024-01-21 19:30:05 +0100

    more express configurations

--- ts/index.ts
+++ ts/index.ts
@@ -1,39 +1,87 @@
-import type { NextFunction, Request, Response } from "express"
-import express from "express"
-import compression from "compression"
-import hpp from "hpp"
-import helmet from "helmet"
-import timeout from "connect-timeout"
-import { rateLimit } from "express-rate-limit"
-import cors from "cors"
-import { StatusCodes } from "http-status-codes"
-import responseTime from "response-time"
-import { ENV, KEYS } from "./src/constant/index.js";
-import { router } from "./src/router/index.js"
-import path from "path"
+import type { NextFunction, Request, Response } from "express";
+import express from "express";
+import compression from "compression";
+import hpp from "hpp";
+import session from "express-session";
+import morgan from "morgan";
+import helmet from "helmet";
+import timeout from "connect-timeout";
+import { rateLimit } from "express-rate-limit";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { StatusCodes } from "http-status-codes";
+import responseTime from "response-time";
+import { ENV, KEYS, VALUES } from "./src/constant/index.js";
+import { config } from "./src/config/index.js";
+import { router } from "./src/router/index.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
-const app = express()
-app.enable("json escape")
-app.set("env", ENV.NODE.ENV)
+const app = express();
+app.disable("x-powered-by");
+app.disable("trust proxy");
+app.enable("json escape");
+app.enable("view cache");
+app.set("env", ENV.NODE.ENV);
 
-app.use(timeout("5s"))
-app.use(responseTime())
-app.use(cors())
-app.use(rateLimit())
-app.use(compression())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet())
-app.use(hpp())
+app.use(timeout("5s"));
+app.use(responseTime());
+app.use(cors());
+app.use(rateLimit());
 
-global.IS_PRODUCTION = ENV.NODE.ENV === "production"
-global.__root = process.cwd()
+global.IS_PRODUCTION = ENV.NODE.ENV === "production";
+global.__filename = fileURLToPath(import.meta.url);
+global.__dirname = path.dirname(__filename);
+global.__root = process.cwd();
 
-app.use("/", router)
-app.use(express.static(path.join(__root, KEYS.GLOBAL.PUBLIC)))
-app.use("*", (_, res) => res.status(StatusCodes.NOT_FOUND))
-app.use((error: Error, _req: Request, _res: Response, next: NextFunction) => next(error))
+if (!IS_PRODUCTION) {
+    const errorhandler = (await import("errorhandler")).default;
+    app.use(errorhandler({ log: true }));
+    await import("colors");
+}
+
+app.use(morgan(IS_PRODUCTION ? "combined" : "dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(compression());
+app.use(cookieParser());
+app.use(helmet());
+app.use(hpp());
+app.use(
+    session({
+        secret: "",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: VALUES.TIME.MONTH,
+            httpOnly: IS_PRODUCTION,
+            sameSite: IS_PRODUCTION,
+            secure: IS_PRODUCTION,
+            path: "/",
+        },
+    })
+);
+
+app.use("/", router);
+app.use(express.static(path.join(__root, KEYS.GLOBAL.PUBLIC)));
+app.use("*", (_, res) => res.status(StatusCodes.NOT_FOUND));
+app.use((error: Error, _req: Request, _res: Response, next: NextFunction) =>
+    next(error)
+);
+
+process.on("unhandledRejection", (error) => {
+    throw error;
+});
+
+process.on("uncaughtException", async (error) => {
+    console.error(error);
+
+    const { db } = config;
+    await db.close();
+
+    process.exit(1);
+});
 
 app.listen(ENV.NODE.PORT, () => {
-    console.log("Listing");
-})
+    if (!IS_PRODUCTION) console.log("Starting".bgGreen.white);
+});
