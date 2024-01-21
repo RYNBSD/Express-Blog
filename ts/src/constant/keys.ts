0245fba367ea5658b21f6439fe2adc06873c297c
RYN BSD <rynbsd04@gmail.com>
2024-01-21 19:07:32 +0100

    modify constant KEYS

--- ts/src/constant/keys.ts
+++ ts/src/constant/keys.ts
@@ -3,20 +3,14 @@ export const KEYS = {
         HEADERS: {
             CSRF: "X-CSRF-Token",
             JWT: "X-JWT-Token",
-            METHOD_OVERRIDE: "X-HTTP-Method-Override",
-            NO_COMPRESSION: "X-No-Compression",
             USER_ID: "X-User-Id",
-            RESPONSE_TIME: "X-Response-Time",
             ACCESS_TOKEN: "Access-Token",
         },
-        METHODS: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     },
     COOKIE: {
         JWT: "jwt_token",
     },
     GLOBAL: {
         PUBLIC: "public",
-        PICTURE: "picture",
-        IMAGE: "image",
     },
 }
