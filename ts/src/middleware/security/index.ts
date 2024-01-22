c299affb5e7a7fdb0687a6566cb49a4e9e7066c7
RYN BSD <rynbsd04@gmail.com>
2024-01-22 14:32:12 +0100

    config csrf middleware

--- ts/src/middleware/security/index.ts
+++ ts/src/middleware/security/index.ts
@@ -1,3 +1,24 @@
+import type { Request, Response, NextFunction } from "express"
+import { util } from "../../util/index.js";
+import { KEYS } from "../../constant/index.js";
+
 export const security = {
+    async csrf(req: Request, _res: Response, next: NextFunction) {
+        const { getHeader, csrf } = util
+
+        const token = getHeader(req.headers, KEYS.HTTP.HEADERS.CSRF);
+        if (token.length === 0)
+            throw new Error("Empty CSRF token");
     
+        if (token instanceof Array)
+            throw new Error("Too many CSRF token");
+    
+        const secret = req.session.csrf?.secret ?? "";
+        if (secret.length === 0)
+            throw new Error("Empty CSRF secret");
+    
+        req.session.csrf = { secret: "" };
+        if (csrf.verify(secret, token)) return next();
+        throw new Error("Invalid CSRF");
+    }
 } as const
