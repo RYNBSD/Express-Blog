3a962d7f86e024ecaaac822e9bd06dd76c264966
RYN BSD <rynbsd04@gmail.com>
2024-01-22 13:13:54 +0100

    add auth controller to router

--- ts/src/router/api/auth.ts
+++ ts/src/router/api/auth.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
+import { util } from "../../util/index.js";
+import { config } from "../../config/index.js";
+import { controller } from "../../controller/index.js";
 
-export const auth = Router()
+export const auth = Router();
 
-auth.post("/sign-up")
+const { handleAsync } = util;
+const { upload } = config
+const { api } = controller
 
-auth.post("/sign-in")
+auth.post("/sign-up", upload.single("picture"), handleAsync(api.auth.signUp));
 
-auth.post("/sign-out")
+auth.post("/sign-in", handleAsync(api.auth.signIn));
 
-auth.post("/me")
+auth.post("/sign-out", handleAsync(api.auth.signOut));
+
+auth.post("/me", handleAsync(api.auth.me));
