7287d379767bdc87d46ed31a16f836f4a21a6604
RYN BSD <rynbsd04@gmail.com>
2024-01-24 13:52:00 +0100

    config access controller

---
+++ ts/src/controller/security/access.ts
@@ -0,0 +1,32 @@
+import type { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { util } from "../../util/index.js";
+import { model } from "../../model/index.js";
+import { lib } from "../../lib/index.js";
+import { KEYS } from "../../constant/index.js";
+
+export const access = {
+    async email(req: Request, res: Response) {
+        const { email } = req.body;
+
+        const { User } = model.db;
+        const user = await User.findOne({
+            attributes: ["id"],
+            where: { email },
+            plain: true,
+            limit: 1,
+        });
+        if (user === null) throw new Error("User not found");
+
+        const { access } = util;
+        const { token, iv, key, code } = access.token(user.dataValues.id);
+        const { Mail } = lib;
+
+        await new Mail(email, "Verify Token", code).send();
+
+        req.session.access = { key, iv };
+        res.status(StatusCodes.CREATED)
+            .setHeader(KEYS.HTTP.HEADERS.ACCESS_TOKEN, token)
+            .end();
+    },
+} as const;
