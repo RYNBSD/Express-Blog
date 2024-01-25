91c273dc16539c8670853d94cb3521284754f266
RYN BSD <rynbsd04@gmail.com>
2024-01-25 11:11:26 +0100

    confgi blog

--- ts/src/controller/api/blog.ts
+++ ts/src/controller/api/blog.ts
@@ -1,3 +1,38 @@
-export const blog = {
+import type { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
-} as const
+export const blog = {
+    async all(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async blog(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async blogLikes(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async blogComments(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async like(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async createBlog(req: Request, res: Response) {
+        res.status(StatusCodes.CREATED).end();
+    },
+    async createComment(req: Request, res: Response) {
+        res.status(StatusCodes.CREATED).end();
+    },
+    async updateBlog(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async updateComment(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async deleteBlog(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+    async deleteComment(req: Request, res: Response) {
+        res.status(StatusCodes.OK).end();
+    },
+} as const;
--- ts/src/router/api/blog.ts
+++ ts/src/router/api/blog.ts
@@ -1,55 +1,83 @@
 import { Router } from "express";
 import { util } from "../../util/index.js";
 import { middleware } from "../../middleware/index.js";
+import { controller } from "../../controller/index.js";
 
 export const blog = Router();
 
 const { handleAsync } = util;
+const { api } = controller;
 
-blog.get("/all", handleAsync(middleware.hasUserUnregistered));
+blog.get(
+    "/all",
+    handleAsync(middleware.hasUserUnregistered),
+    handleAsync(api.blog.all)
+);
 
-blog.get("/:blogId", handleAsync(middleware.hasUserUnregistered));
+blog.get(
+    "/:blogId",
+    handleAsync(middleware.hasUserUnregistered),
+    handleAsync(api.blog.blog)
+);
 
-blog.get("/:blogId/likes", handleAsync(middleware.hasUserUnregistered));
+blog.get(
+    "/:blogId/likes",
+    handleAsync(middleware.hasUserUnregistered),
+    handleAsync(api.blog.blogLikes)
+);
 
-blog.get("/:blogId/comments", handleAsync(middleware.hasUserUnregistered));
+blog.get(
+    "/:blogId/comments",
+    handleAsync(middleware.hasUserUnregistered),
+    handleAsync(api.blog.blogComments)
+);
 
-blog.patch("/:blogId/like", handleAsync(middleware.hasUserRegistered));
+blog.patch(
+    "/:blogId/like",
+    handleAsync(middleware.hasUserRegistered),
+    handleAsync(api.blog.like)
+);
 
 blog.post(
     "/",
     handleAsync(middleware.security.csrf),
-    handleAsync(middleware.hasUserRegistered)
+    handleAsync(middleware.hasUserRegistered),
+    handleAsync(api.blog.createBlog)
 );
 
 blog.post(
     "/:blogId/comment",
     handleAsync(middleware.security.csrf),
-    handleAsync(middleware.hasUserRegistered)
+    handleAsync(middleware.hasUserRegistered),
+    handleAsync(api.blog.createComment)
 );
 
 blog.put(
     "/:blogId",
     handleAsync(middleware.security.csrf),
     handleAsync(middleware.hasUserRegistered),
-    handleAsync(middleware.api.blog.isBlogOwner)
+    handleAsync(middleware.api.blog.isBlogOwner),
+    handleAsync(api.blog.updateBlog)
 );
 
 blog.put(
     "/:blogId/:commentId",
     handleAsync(middleware.security.csrf),
     handleAsync(middleware.hasUserRegistered),
-    handleAsync(middleware.api.blog.isCommentOwner)
+    handleAsync(middleware.api.blog.isCommentOwner),
+    handleAsync(api.blog.updateComment)
 );
 
 blog.delete(
     "/:blogId",
     handleAsync(middleware.hasUserRegistered),
-    handleAsync(middleware.api.blog.isBlogOwner)
+    handleAsync(middleware.api.blog.isBlogOwner),
+    handleAsync(api.blog.deleteBlog)
 );
 
 blog.delete(
     "/:blogId/:commentId",
     handleAsync(middleware.hasUserRegistered),
-    handleAsync(middleware.api.blog.isCommentOwner)
+    handleAsync(middleware.api.blog.isCommentOwner),
+    handleAsync(api.blog.deleteComment)
 );
